427be95130d51cdffb6c1755a16d745fd5d8f2e3
El Mehdi Cherkaoui <cherkaouim451@gmail.com>
2026-03-18 10:09:25 +0000

    feat : implement ProductController for managing products, including listing, showing, storing, updating, and deleting

---
+++ web/app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Product;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $products = Product::with(['category', 'productImage'])->where('stock', '>', 0)->get();
+
+        return response()->json($products, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($slug)
+    {
+        $product = Product::with(['category', 'productImage'])
+            ->where('slug', $slug)
+            ->first();
+
+        if (!$product) {
+            return response()->json([
+                'message' => 'Product not found',
+            ], 404);
+        }
+
+        return response()->json($product, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
