57d0d0365dc9912cfab447fabf8b773bfe003c0e
El Mehdi Cherkaoui <cherkaouim451@gmail.com>
2026-03-19 19:52:17 +0000

    refactor: update AuthController to use DTOs for user registration and login

--- web/app/Http/Controllers/Api/AuthController.php
+++ web/app/Http/Controllers/Api/AuthController.php
@@ -2,26 +2,38 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\DTO\LoginDTO;
+use App\DTO\RegisterDTO;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\RegisterRequest;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use PHPOpenSourceSaver\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
     {
+        $validated = $request->validated();
+
+        $dto = new RegisterDTO(
+            $validated['name'],
+            $validated['email'],
+            $validated['password'],
+            $validated['role'],
+        );
+
         $user = User::create([
-                'name' => $request->name,
-                'email' => $request->email,
-                'password' => $request->password,
-                'role' => $request->role,
+            'name' => $dto->name,
+            'email' => $dto->email,
+            'password' => Hash::make($dto->password),
+            'role' => $dto->role,
         ]);
 
         $token = JWTAuth::attempt([
-            'email' => $request->email,
-            'password' => $request->password,
+            'email' => $dto->email,
+            'password' => $dto->password,
         ]);
 
         return response()->json([
@@ -30,11 +42,22 @@ class AuthController extends Controller
             'token' => $token,
         ], 201);
     }
+
     public function login(LoginRequest $request)
     {
-        $credentials = $request->only('email', 'password');
+        $validated = $request->validated();
 
-        if (!$token = JWTAuth::attempt($credentials)) {
+        $dto = new LoginDTO(
+            email: $validated['email'],
+            password: $validated['password'],
+        );
+
+        $credentials = [
+            'email' => $dto->email,
+            'password' => $dto->password,
+        ];
+
+        if (! $token = JWTAuth::attempt($credentials)) {
             return response()->json([
                 'message' => 'Invalid credentials',
             ], 401);
@@ -46,6 +69,7 @@ class AuthController extends Controller
             'token' => $token,
         ], 200);
     }
+
     public function me()
     {
         return response()->json([
