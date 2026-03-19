0fbfd0bd2462a73752220855d7cb82841fe9c256
El Mehdi Cherkaoui <cherkaouim451@gmail.com>
2026-03-19 20:11:36 +0000

    feat: add slug generation functionality to Category model

--- web/app/Models/Category.php
+++ web/app/Models/Category.php
@@ -4,16 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Category extends Model
 {
-    use HasFactory;
+    use HasFactory, HasSlug;
 
     protected $fillable = [
         'name',
         'slug',
         'description',
     ];
+
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
 
     public function products()
     {
